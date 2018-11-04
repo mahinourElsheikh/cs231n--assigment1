48935051ca602b13a3b1018822680cc69e3ac60f
MahinourElsheikh <mahinour-elsheikh@hotmail.com>
2018-11-04 23:12:58 +0200

    nntrain  added  (Group work)

--- assignment1/cs231n/classifiers/neural_net.py
+++ assignment1/cs231n/classifiers/neural_net.py
@@ -191,7 +191,9 @@ class TwoLayerNet(object):
       # TODO: Create a random minibatch of training data and labels, storing  #
       # them in X_batch and y_batch respectively.                             #
       #########################################################################
-      pass
+      batch_indices = np.random.choice(X.shape[0],32)
+      X_batch = X[batch_indices]
+      y_batch = y[batch_indices]
       #########################################################################
       #                             END OF YOUR CODE                          #
       #########################################################################
@@ -206,7 +208,10 @@ class TwoLayerNet(object):
       # using stochastic gradient descent. You'll need to use the gradients   #
       # stored in the grads dictionary defined above.                         #
       #########################################################################
-      pass
+      self.params['W1'] += -learning_rate* grads['W1']
+      self.params['W2'] += -learning_rate* grads['W2']
+      self.params['b1'] += -learning_rate* grads['b1']
+      self.params['b2'] += -learning_rate* grads['b2']
       #########################################################################
       #                             END OF YOUR CODE                          #
       #########################################################################
