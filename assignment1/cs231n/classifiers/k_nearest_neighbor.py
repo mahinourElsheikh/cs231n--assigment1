22989967caaa2c31f21b84a91838bb234c4cd368
MahinourElsheikh <mahinour-elsheikh@hotmail.com>
2018-11-04 22:54:27 +0200

    L2 distance added

--- assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -71,7 +71,7 @@ class KNearestNeighbor(object):
         # training point, and store the result in dists[i, j]. You should   #
         # not use a loop over dimension.                                    #
         #####################################################################
-        pass
+        dists[i][j]= np.sqrt(np.sum(np.power((X[i]-self.X_train[j]),2)))
         #####################################################################
         #                       END OF YOUR CODE                            #
         #####################################################################
